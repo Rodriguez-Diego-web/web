6a521ae3154b8666cb2338ce9f693f9261d219eb
Diego Rodriguze <diego@rodriguez-digital.de>
2025-05-25 14:20:03 +0200

    Fix: Füge rodriguez-web.de zur Image Optimizer Domain-Liste hinzu, um 404-Fehler bei Bildern zu beheben

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost', 'ui-avatars.com', 'maps.googleapis.com', 'lh3.googleusercontent.com', 'lh5.googleusercontent.com'],
+    domains: ['localhost', 'ui-avatars.com', 'maps.googleapis.com', 'lh3.googleusercontent.com', 'lh5.googleusercontent.com', 'rodriguez-web.de'],
   },
 }
 
